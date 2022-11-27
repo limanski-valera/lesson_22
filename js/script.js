fe0e4eb4f2b66ce7e27221e038b112404ffc110b
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2022-11-27 22:17:16 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Домашка по другому відео:
+"use strict";
+console.log('Я вчу JS');
+// Помилки:
+// Варіант №1 - помилки відсутні
+console.log('Учим JS');
+
+// Варіант №2 - помилки відсутні
+console.log('JS')
+console.log('Учим')
+
+// Варіант №3 - помилка! 
+// console.log('JS') console.log('Учим')
+
+// Варіант №4 - помилка! 
+// console.log('Учим);
+// console.log('JS');
+
+console.log('============================================')
+
+// Домашка по третьому відео:
+let userEyeColor = '#000000';
+
+let user,
+    userName = 'Вася';
+
+user = userName;
+
+console.log(user);
+
+// Помилки:
+// Варіант №1 - помилка
+// let user = 'Вася' let age = 36
+
+// Варіант №2 - вірно
+let myage = 36;
+
+console.log('============================================')
+// Домашка по четвертому відео:
+let userAge = 36;
+let userInfo = "Фрілансер ${userAge}";
+console.log(userInfo);
+
+// Варіант 2
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+// Варіант 3
+let secondUserName;
+console.log(secondUserName);
+
+// Варіант 4
+let userSize = "45" / "8";
+console.log(typeof userSize);
